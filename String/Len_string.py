8edb2b9b565a3c8286ae1932049a7e6b4c671cda
Kaustubh Kale <kaustubh.mum.dbda@gmail.com>
2024-04-23 12:03:55 +0530

    [Kaustubh Kale] Add : Python script to print the length of the string

---
+++ String/Len_string.py
@@ -0,0 +1,11 @@
+def len_string(inp_string):
+    return len(inp_string)
+
+def main():
+    inp_string=input("Enter the string : ")
+    len=len_string(inp_string)
+    print(f"The length of the input string is : {len}")
+
+
+if __name__=="__main__":
+    main()
