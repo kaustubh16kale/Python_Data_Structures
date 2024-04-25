0615497f1fd1847b1d5666752b841554c78161ab
Kaustubh Kale <kaustubh.mum.dbda@gmail.com>
2024-04-25 12:21:58 +0530

    [Kaustubh Kale] Add: Python script to return symmetric_difference

---
+++ Set/9_Symmetric_Difference.py
@@ -0,0 +1,16 @@
+def symmetric_difference(set1,set2):
+    return set1.symmetric_difference(set2)
+
+def main():
+    try:
+        set1=input("Enter the values to be added in set1: ")
+        set2=input("Enter the values to be added in set2: ")
+        set1=set(set1.split(','))
+        set2=set(set2.split(','))
+        print(symmetric_difference(set1,set2))
+    except Exception as e:
+        print(e)
+
+
+if __name__=="__main__":
+    main()
