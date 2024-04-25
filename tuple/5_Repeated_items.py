8bb4096ee8e8001dda0b6f149ee46963111300c5
Kaustubh Kale <kaustubh.mum.dbda@gmail.com>
2024-04-25 11:20:46 +0530

    [Kaustubh Kale] Add: Python script to find repeated values in the tuple

---
+++ tuple/5_Repeated_items.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+def repeated_items(inp_tuple):
+    new_list=[]
+    count_items=Counter(inp_tuple)
+    print(count_items)
+    for key,value in count_items.items():
+        if value>1:
+            new_list.append(i)
+    return new_list
+    # return [x for x,count in count_items.items() if count>1]
+
+def main():
+    inp_tuple=input("Enter the values to be added in the tuple seperated by ',' : ")
+    inp_tuple=inp_tuple.split(',')
+    print(repeated_items(inp_tuple))
+
+
+if __name__=="__main__":
+    main()
